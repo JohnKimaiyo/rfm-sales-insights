40a450aa867bcf43fa71685303e23ec5ec68de9f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-10-17 10:18:16 +0300

    select tables

--- SQL Scripts/sales.sql
+++ SQL Scripts/sales.sql
@@ -8,3 +8,22 @@ select distinct PRODUCTLINE from [dbo].[sales_data_sample] ---Nice to plot
 select distinct COUNTRY from [dbo].[sales_data_sample] ---Nice to plot
 select distinct DEALSIZE from [dbo].[sales_data_sample] ---Nice to plot
 select distinct TERRITORY from [dbo].[sales_data_sample] ---Nice to plot
+
+
+---ANALYSIS
+----Let's start by grouping sales by productline
+select PRODUCTLINE, sum(sales) Revenue
+from [dbo].[sales_data_sample]
+group by PRODUCTLINE
+order by 2 desc
+
+
+select YEAR_ID, sum(sales) Revenue
+from [dbo].[sales_data_sample]
+group by YEAR_ID
+order by 2 desc
+
+select  DEALSIZE,  sum(sales) Revenue
+from [dbo].[sales_data_sample]
+group by  DEALSIZE
+order by 2 desc
